ad4ec5a792ea6349100d929560eed66c771ebb67
Thibault Touzin <thibault.touzin@gmail.com>
2022-07-06 10:50:02 +0200

    orm comments

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -2,11 +2,14 @@
 
 namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
-
+//on utilise doctrine pour créer/trouver/migrer bdd
 /**
  * @ORM\Entity()
  */
+//ici symfony capte grâce à orm qu' entity n'est pas une route mais un outil pour la transformation en bdd
+//on crée un entitée class article grâce à orm
 class Article
+//article sera une table
 {
     /**
      * @ORM\Column(type="integer")
@@ -14,9 +17,11 @@ class Article
      * @ORM\GeneratedValue()
      */
     public $id;
+    //id sera un champ de la table
 
     /**
      * @ORM\Column(type="string")
      */
     public $title;
+    //title aussi
 }
