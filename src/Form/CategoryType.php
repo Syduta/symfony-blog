b8b6d03864166ac9f7bcd98ccdd7ca455cb17d19
Thibault Touzin <thibault.touzin@gmail.com>
2022-07-12 16:11:29 +0200

    bootstraping form

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ColorType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,7 +15,7 @@ class CategoryType extends AbstractType
     {
         $builder
             ->add('title')
-            ->add('color')
+            ->add('color', ColorType::class)
             ->add('description')
             ->add('isPublished')
             ->add('submit',SubmitType::class)
